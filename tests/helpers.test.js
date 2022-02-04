ce2647e07c806d85d985d9ddb8f68ecbca84f738
confused-Techie <dev@lhbasics.com>
2022-02-03 19:46:23 -0800

    Additional Testing

--- tests/helpers.test.js
+++ tests/helpers.test.js
@@ -3,3 +3,7 @@ const helpers = require("../src/parsers/helpers.js");
 test("Passes 'Comment' to gitLikeAddComment to get '# Comment'", () => {
   expect(helpers.gitLikeAddComment("Comment")).toBe("# Comment\n");
 });
+
+test("Passes 'Thisisastring' to isNotNull to get 'true'", () => {
+  expect(helpers.isNotNull("Thisisastring")).toBe(true);
+});
