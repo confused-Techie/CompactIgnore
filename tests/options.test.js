655038a9a64192c96baa25f7406022174d79e433
confused-Techie <dev@lhbasics.com>
2022-02-04 17:21:07 -0800

    Fixed testing operator

--- tests/options.test.js
+++ tests/options.test.js
@@ -3,5 +3,5 @@ const options = require("../src/cli/options.js");
 // Since options.js is unfinished we only need one test
 test("Ensures true is returned as status obj, options is unfinished", () => {
   var testObj = { status: true };
-  expect(options.parseArgv("anything")).toBe(testObj);
+  expect(options.parseArgv("anything")).toStrictEqual(testObj);
 });
