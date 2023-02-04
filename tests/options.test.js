84ccb2cb32672905c419e97dda9f3ee4532f19e5
confused-Techie <dev@lhbasics.com>
2023-02-03 17:32:03 -0800

    New tests, new GitHub Actions

--- tests/options.test.js
+++
@@ -1,7 +0,0 @@
-const options = require("../src/cli/options.js");
-
-// Since options.js is unfinished we only need one test
-test("Ensures true is returned as status obj, options is unfinished", () => {
-  var testObj = { status: true };
-  expect(options.parseArgv("anything")).toStrictEqual(testObj);
-});
